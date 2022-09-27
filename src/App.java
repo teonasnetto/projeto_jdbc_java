1194ff445c3843bdcc7757c315d107bf9d44e102
teonasnetto <teonasnetto@gmail.com>
2022-09-27 17:05:51 -0300

    Seller entity class

--- src/App.java
+++ src/App.java
@@ -1,9 +1,14 @@
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "bob@bob.com", new Date(), 3000.0, obj);
 
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
